871dc2e48dceb1ee04b32cab0405c032b13cdfc2
App Academy Student <student@appacademy.io>
2017-01-27 13:35:22 -0800

    Finish tree_node exercise

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,53 @@
+require 'byebug'
+
 class PolyTreeNode
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  attr_reader :parent, :children, :value
+
+  def parent=(parent_node)
+
+    @parent.children.delete(self) unless @parent.nil?
+    @parent = parent_node
+    @parent.children << self unless @parent.nil? || @parent.children.include?(self)
+
+    @parent
+  end
+
+  def add_child(child_node)
+    unless @children.include?(child_node)
+      @children << child_node
+      child_node.parent = self
+    end
+  end
+
+  def remove_child(child_node)
+    raise unless @children.include?(child_node)
+    child_node.parent = nil
+    @children.delete(child_node)
+  end
+
+  def dfs(target_value)
+    @children.each do |child|
+      result = child.dfs(target_value)
+      return result unless result.nil?
+    end
+    return self if @value == target_value
+    nil
+  end
+
+  def bfs(target_value)
+    queue = [self]
+    until queue.empty?
+      node = queue.shift
+      return node if node.value == target_value
+      queue += node.children
+    end
+    nil
+  end
 
 end
