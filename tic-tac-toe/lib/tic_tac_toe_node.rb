4635ea445999853f71abb47a9b963e7ed9440718
App Academy Student <student@appacademy.io>
2017-01-27 16:34:09 -0800

    Add children method to allow setting children

--- tic-tac-toe/lib/tic_tac_toe_node.rb
+++ tic-tac-toe/lib/tic_tac_toe_node.rb
@@ -1,7 +1,14 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
+    @root =
+    @parent = nil
   end
 
   def losing_node?(evaluator)
@@ -13,5 +20,32 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    children_arr = []
+    empty_pos = []
+    @board.rows.each_index do |i|
+      @board.rows[i].each_index do |j|
+        pos = [i, j]
+        empty_pos << [i, j] if @board.empty?(pos)
+      end
+    end
+
+    switch_mark
+
+    duped_board = @board.dup
+    empty_pos.each do |pos|
+      duped_board[pos] = @next_mover_mark
+      @prev_mov_pos = pos
+      children_arr << TicTacToeNode.new(duped_board, @next_mover_mark, @prev_mov_pos)
+    end
+
+    children_arr
+
   end
+
+  def switch_mark
+    @next_mover_mark = (@next_mover_mark == :x ? :o : :x)
+  end
+
+  
+
 end
